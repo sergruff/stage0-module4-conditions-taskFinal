14e5513a2fa20fd7effc063a305eab6bd019f7d8
sergruff <sergruff@gmail.com>
2023-02-23 01:51:16 +0100

    s0m4taskFinal

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        if (x==0&&y==0){
+            System.out.println("zero");
+            return;
+        }
+        if (x>0){
+            System.out.println(y>0?"first":"fourth");
+        } else System.out.println(y>0?"second":"third");
     }
 }
